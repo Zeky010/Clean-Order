04b8f7f814ab28b054ecf7a69decf179170c17c5
Zeky010 <39737796+Zeky010@users.noreply.github.com>
2025-10-27 16:01:35 -0300

    Se agrega script de regiones y comuna completas

--- SQL/data.sql
+++ SQL/data.sql
@@ -1,14 +1,3 @@
-INSERT INTO `gestion-ot`.region (CODIGO,NOMBRE) VALUES
-	 (9,' REGIÓN DE LA ARAUCANÍA'),
-	 (13,' REGIÓN METROPOLITANA DE SANTIAGO');
-	 
-INSERT INTO `gestion-ot`.comuna (CODIGO,NOMBRE,FK_CODIGO_REGION) VALUES
-	 (9101,'Temuco',9),
-	 (9102,'Carahue',9),
-	 (9103,'Cunco',9),
-	 (13101,'Santiago',13),
-	 (13102,'Cerrillos',13),
-	 (13103,'Cerro Navia',13);
 
 INSERT INTO `gestion-ot`.rol (NOMBRE) VALUES
 	 ('ADMIN');
